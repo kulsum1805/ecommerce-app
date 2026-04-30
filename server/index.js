8676fb57694706db419b5da89a24f858026f1e27
kulsum1805 <kulsumaiman752@gmail.com>
2026-04-30 20:12:06 +0530

    fix port for render

--- server/index.js
+++ server/index.js
@@ -1,18 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
-
-const connectDB = require("./config/db");
+require("dotenv").config({ path: "../.env" });
 
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 
-// Connect DB
-connectDB();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -20,13 +15,17 @@ app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
+    );
+  })
+  .catch((err) => console.error(err));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  app.listen(process.env.PORT || 10000, () =>
+  console.log(`Server running on port ${process.env.PORT || 10000}`)
+);
